7e0ab304fd6188d58a64b101e487d567dda7c6a2
gaizhaozhao <19983404540@163.com>
2024-01-30 10:34:40 +0800

    反转字符串

---
+++ 5.js
@@ -0,0 +1,8 @@
+var reverseString = function(s) {
+    let str,temp
+    for(let i = 0;i < s.length/2;i++){
+        temp = s[i]
+        s[i] = s[s.length-i-1]
+        s[s.length-i-1] = temp
+    }
+};
